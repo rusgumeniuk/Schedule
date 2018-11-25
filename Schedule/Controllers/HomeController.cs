7ba07926bd0f0b882ce2561286bbb671a2435551
rusgumeniuk <rus.gumeniuk@gmail.com>
2018-11-25 22:54:11 +0200

    ref

--- Schedule/Controllers/HomeController.cs
+++ Schedule/Controllers/HomeController.cs
@@ -1,16 +1,41 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Schedule.Models;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 
 namespace Schedule.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IList<Teacher> Teachers;
+        private readonly IList<Group> Groups;
+
+        public HomeController()
+        {
+            this.Teachers = new List<Teacher>() { new Teacher() { Title = "First Teacher" }, new Teacher() { Title = "Second Teacher" } };
+            this.Groups = new List<Group>() { new Group() { Title = "First Group" }, new Group() { Title = "Second Group" } };
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult GroupFind()
+        {
+            return View(this.Groups);
+        }
+        public IActionResult TeacherFind()
+        {
+            return View(this.Teachers);
+        }
+
+        [HttpPost]
+        public IActionResult GroupSchedule(Group group)
+        {
+            return View(group);
+        }
+        public IActionResult TeacherSchedule(Teacher teacher)
+        {
+            return View(teacher);
         }
     }
 }
