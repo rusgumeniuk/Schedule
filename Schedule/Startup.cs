e39c18d8e3f24d6fc40f5b23399b7abdeba266aa
rusgumeniuk <rus.gumeniuk@gmail.com>
2018-11-25 21:15:55 +0200

    refact

--- Schedule/Startup.cs
+++ Schedule/Startup.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -12,21 +7,17 @@ namespace Schedule
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-                
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();                
-            }                           
+                app.UseDeveloperExceptionPage();
+            }
 
             app.UseMvc(routes =>
             {
@@ -34,7 +25,7 @@ namespace Schedule
                 name: "default",
                 template: "{controller=Home}/{action=Index}/{id?}"
                 );
-            });              
+            });
         }
     }
 }
