2e9ff7fe0ac05a57bdb5e731259ef0ac02f4a72f
rusgumeniuk <rus.gumeniuk@gmail.com>
2018-11-26 23:26:20 +0200

    Created SortLessons()

---
+++ Schedule/Models/ScheduleFormatter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Schedule.Models
+{
+    public static class ScheduleFormatter
+    {
+        public static IEnumerable<Lesson> SortLessons(IEnumerable<Lesson> lessons)
+        {
+            Array.Sort(lessons.ToArray(), new LessonComparer());
+            return lessons;
+        }
+    }
+}
