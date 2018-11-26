edad7b43cf248e072d45db907a31ec9b04f6a827
rusgumeniuk <rus.gumeniuk@gmail.com>
2018-11-26 23:25:11 +0200

    Reafactored methods which work with info about selected group/teacher from forms

--- Schedule/Controllers/HomeController.cs
+++ Schedule/Controllers/HomeController.cs
@@ -1,18 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Schedule.Models;
+using System;
 using System.Collections.Generic;
 
 namespace Schedule.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly IList<Teacher> Teachers;
-        private readonly IList<Group> Groups;
-
-        public HomeController()
+        static HomeController()
         {
-            this.Teachers = new List<Teacher>() { new Teacher() { Title = "First Teacher" }, new Teacher() { Title = "Second Teacher" } };
-            this.Groups = new List<Group>() { new Group() { Title = "First Group" }, new Group() { Title = "Second Group" } };
+            Teacher firstTeacher = new Teacher() { Title = "First Teacher" };
+            Teacher secondTeacher = new Teacher() { Title = "Second Teacher" };
+
+            Group firstGroup = new Group() { Title = "First Group" };
+            Group it62 = new Group() { Title = "IT-62" };
+            Group it52 = new Group() { Title = "IA-52" };
+
+            Subject math = new Subject() { Title = "Math" };
+            Subject english = new Subject() { Title = "English" };
+
+            Build fict = new Build() { };
+            Room mathLectureRoom = new Room(fict) { Number = 303 };
+            Room mathPractRoom = new Room(fict) { Number = 428 };
+            Room engLectureRoom = new Room(fict) { Number = 506 };
+            Room englishPractRoom = new Room(fict) { Number = 506 };
+
+            Lesson math62FirstLecture = new Lesson(math.Id, firstTeacher.Id, it62.Id, mathLectureRoom.Id,
+                Models.Enums.WeekMode.Both, DayOfWeek.Monday, Models.Enums.LessonNumber.First, Models.Enums.LessonType.Lecture);
+            Lesson math62FirstPract = new Lesson(math.Id, firstTeacher.Id, it62.Id, mathPractRoom.Id,
+                Models.Enums.WeekMode.First, DayOfWeek.Monday, Models.Enums.LessonNumber.Second, Models.Enums.LessonType.Practice);
+
+            Lesson eng62Pract = new Lesson(english.Id, secondTeacher.Id, it62.Id, englishPractRoom.Id,
+                Models.Enums.WeekMode.Second, DayOfWeek.Monday, Models.Enums.LessonNumber.Second, Models.Enums.LessonType.Practice);
+            Lesson eng62Lecture = new Lesson(english.Id, secondTeacher.Id, it62.Id, engLectureRoom.Id,
+                Models.Enums.WeekMode.Both, DayOfWeek.Tuesday, Models.Enums.LessonNumber.First, Models.Enums.LessonType.Lecture);
         }
         public IActionResult Index()
         {
@@ -21,21 +42,22 @@ namespace Schedule.Controllers
 
         public IActionResult GroupFind()
         {
-            return View(this.Groups);
+            return View(Group.Items.Values);
         }
         public IActionResult TeacherFind()
         {
-            return View(this.Teachers);
+            return View(Teacher.Items.Values);
         }
 
         [HttpPost]
-        public IActionResult GroupSchedule(Group group)
-        {
-            return View(group);
+        public IActionResult GroupSchedule(Guid id)
+        {            
+            return View(Group.Items[id]);
         }
-        public IActionResult TeacherSchedule(Teacher teacher)
+        [HttpPost]
+        public IActionResult TeacherSchedule(Guid id)
         {
-            return View(teacher);
+            return View(Teacher.Items[id]);
         }
     }
 }
