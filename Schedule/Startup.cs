74bb4b34b7ff1769ecbd5e9d21ce8e0439a85308
rusgumeniuk <rus.gumeniuk@gmail.com>
2018-11-30 20:47:33 +0200

    Set up for db

--- Schedule/Startup.cs
+++ Schedule/Startup.cs
@@ -2,13 +2,32 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+using Schedule.Models.Contexts;
 
 namespace Schedule
 {
     public class Startup
     {
-        public void ConfigureServices(IServiceCollection services)
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
         {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {            
+            string connection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<BuildContext>(options => options.UseSqlServer(connection));
+            services.AddDbContext<RoomContext>(options => options.UseSqlServer(connection));
+            services.AddDbContext<TeacherContext>(options => options.UseSqlServer(connection));
+            services.AddDbContext<SubjectContext>(options => options.UseSqlServer(connection));
+            services.AddDbContext<GroupContext>(options => options.UseSqlServer(connection));
+            services.AddDbContext<LessonContext>(options => options.UseSqlServer(connection));
+
             services.AddMvc();
         }
 
