ad10641b7cc2d22fd84029700b6be7c58b6bd4ca
rusgumeniuk <rus.gumeniuk@gmail.com>
2018-12-06 22:53:44 +0200

    Recreated Building instead of Build

---
+++ Schedule/Models/Building.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Schedule.Models
+{
+    public class Building : Base
+    {
+        public readonly ushort MAX_COUNT_OF_ROOMS_ON_STOREY = 600;
+        public byte CountOfStoreys { get; set; } = 4;
+        public byte Number { get; set; }
+        public IList<Room> Rooms { get; set; } = new List<Room>();
+
+        public Building(byte number) : base()
+        {
+            Number = number;
+        }
+        public Building(byte number, IEnumerable<Room> rooms) : this(number)
+        {
+            Rooms = rooms as List<Room>;
+        }
+
+        public void AddRoom(Room room)
+        {
+            if (Rooms.Contains(room))
+                return;
+            else
+                Rooms.Add(room);
+        }
+        public override bool Equals(object obj)
+        {
+            return obj != null && (obj is Building) && Number == (obj as Building).Number;
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, Number);
+        }
+    }
+}
