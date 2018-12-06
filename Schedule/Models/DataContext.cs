dd39e131434f5213a6b4e11cdc76ad59bacbfbea
rusgumeniuk <rus.gumeniuk@gmail.com>
2018-12-06 22:50:52 +0200

    Create DataContext.cs

---
+++ Schedule/Models/DataContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Schedule.Models;
+namespace Schedule.Models
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<Building> Buildings { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<Lesson> Lessons { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server = (localdb)\mssqllocaldb; Database = scheduledb; Trusted_Connection = True;");
+        }
+    }
+}
