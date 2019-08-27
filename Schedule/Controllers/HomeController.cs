b16356dbf9ddf0bfc04fc2aff8fa93f5fa404339
rusgumeniuk <rus.gumeniuk@gmail.com>
2019-08-27 14:36:03 +0300

    Changed Teacher/GroupSchedue on Teacher/GroupFind on post requests. Changed exception types in GetGroup/Teacher, GetLessons

--- Schedule/Controllers/HomeController.cs
+++ Schedule/Controllers/HomeController.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Schedule.Models;
-using Schedule.Models.Enums;
 using Schedule.Models.JsonHelpers;
 using Schedule.ViewModels;
 using System;
@@ -22,42 +21,60 @@ namespace Schedule.Controllers
         public HomeController(DataContext dataContext, IHttpClientFactory _clientFactory)
         {
             db = dataContext;
-            clientFactory = _clientFactory;            
+            clientFactory = _clientFactory;
         }
         public IActionResult Index()
         {
             return View();
         }
 
-        public async Task<IActionResult> GroupFind()
+        [HttpGet]
+        public IActionResult GroupFind()
         {
-            return View(new GroupFindViewModel() { Groups = await GetAllGroups() });
+            return View(new GroupFindViewModel());
         }
-        public async Task<IActionResult> TeacherFind()
+        [HttpGet]
+        public IActionResult TeacherFind()
         {
-            var res = await GetAllTeachers();
-            return View(new TeacherFindViewModel() { Teachers = res });
+            return View(new TeacherFindViewModel());
         }
 
         [HttpPost]
-        public async Task<IActionResult> GroupSchedule(string inputedName)
-        {            
-            return View(new GroupScheduleViewModel() { Lessons = await GetScheduleForGroup(inputedName), Group = await GetGroup(inputedName) });
-        }
-        [HttpPost]
-        public async Task<IActionResult> TeacherSchedule(string inputedName)
+        public async Task<IActionResult> TeacherFind(string inputedName)
         {
             try
             {
-                var res = await GetScheduleForTeacher(inputedName);
-                return View(new TeacherScheduleViewModel() { Lessons = res, Teacher = await GetTeacher(inputedName) });
+                return View("TeacherSchedule", new TeacherScheduleViewModel() { Teacher = await GetTeacher(inputedName), Lessons = await GetScheduleForTeacher(inputedName) });//save order!
+            }
+            catch (ArgumentNullException)
+            {
+                ModelState.AddModelError("SelectedTeacher", "Перевірте ПІБ викладача або виберіть зі списку");
             }
             catch (ArgumentException)
             {
-                ModelState.AddModelError("Lessons", $"Selected teacher with name #{inputedName} has not lessons");
-                return RedirectToAction("TeacherFind");
+                ModelState.AddModelError("SelectedTeacher", $"На даний момент {inputedName} не має навчальних пар");
             }
-        }      
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> GroupFind(string inputedName)
+        {
+            try
+            {
+                if (!IsFullGroupName(inputedName))
+                    throw new ArgumentNullException();
+                return View("GroupSchedule", new GroupScheduleViewModel() { Group = await GetGroup(inputedName), Lessons = await GetScheduleForGroup(inputedName) }); //save order!
+            }
+            catch (ArgumentNullException)
+            {
+                ModelState.AddModelError("SelectedGroup", "Перевірте назву введеної групи або виберіть групу зі списку");
+            }
+            catch (ArgumentException)
+            {
+                ModelState.AddModelError("SelectedGroup", $"Вибрана група {inputedName} не має навчальних пар");
+            }
+            return View();
+        }
 
 
         private async Task<ResponseGroupData> GetGroup(int id)
@@ -74,7 +91,7 @@ namespace Schedule.Controllers
             if (response.IsSuccessStatusCode)
                 return JsonConvert.DeserializeObject<ResponseRootSingeData<ResponseGroupData>>(response.Content.ReadAsStringAsync().Result).Data;
             else
-                throw new ArgumentException($"Not found Group with name {name}");
+                throw new ArgumentNullException($"Not found Group with name {name}");
         }
         private async Task<ResponseTeacherData> GetTeacher(int id)
         {
@@ -90,24 +107,7 @@ namespace Schedule.Controllers
             if (response.IsSuccessStatusCode)
                 return JsonConvert.DeserializeObject<ResponseRootSingeData<ResponseTeacherData>>(response.Content.ReadAsStringAsync().Result).Data;
             else
-                throw new ArgumentException($"Not found Teacher with id {name}");
-        }
-
-        private async Task<IEnumerable<ResponseGroupData>> GetGroupsByName(string name)
-        {
-            var response = await GetResponse($"{baseUrl}groups/?search={{'query':'{name}'}}");
-            if (IsFullGroupName(name))
-            {
-                try
-                {
-                    return new ResponseGroupData[] { JsonConvert.DeserializeObject<ResponseRootSingeData<ResponseGroupData>>(response.Content.ReadAsStringAsync().Result).Data };
-                }
-                catch (Exception)
-                {
-                    //
-                }
-            }
-            return JsonConvert.DeserializeObject<ResponseRootMultipleData<ResponseGroupData>>(response.Content.ReadAsStringAsync().Result).Data;
+                throw new ArgumentNullException($"Not found Teacher with id {name}");
         }
 
         private async Task<IList<ResponseGroupData>> GetAllGroups()
@@ -145,7 +145,8 @@ namespace Schedule.Controllers
             {
                 return JsonConvert.DeserializeObject<ResponseRootMultipleData<ResponseLessonDataForGroup>>(response.Content.ReadAsStringAsync().Result).Data.OrderBy(lesson => lesson.DayNumber).ThenBy(lesson => lesson.LessonNumber).ToArray();
             }
-            return null;
+            else
+                throw new ArgumentException($"Group {name} has not lessons");
         }
         private async Task<IList<ResponseLessonDataForTeacher>> GetScheduleForTeacher(int teacherId)
         {
@@ -179,9 +180,8 @@ namespace Schedule.Controllers
                 return await client.SendAsync(request);
             }
         }
-
         private bool IsFullGroupName(string groupName)
-        {                        
+        {
             return new Regex(@"^\w{2}-[1-9]{2}", RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.IgnoreCase | RegexOptions.Singleline).IsMatch(groupName);
         }
     }
