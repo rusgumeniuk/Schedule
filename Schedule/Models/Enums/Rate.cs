3738d8160a0ed26ba023b7a623b0174f5c09fa9d
rusgumeniuk <rus.gumeniuk@gmail.com>
2019-08-29 22:45:22 +0300

    Create Rate enum to rate teacher and subject

---
+++ Schedule/Models/Enums/Rate.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Schedule.Models.Enums
+{
+    public enum Rate : byte
+    {
+        [Display(Name = "Unknown")]
+        Undefined,        
+        Bad,
+        [Display(Name = "Not good")]
+        NotGood,
+        Normal,
+        Good,
+        [Display(Name = "Very well")]
+        VeryWell
+    }
+}
