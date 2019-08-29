2999f7b153130c68c90f8ae434ac9171ab2687b9
rusgumeniuk <rus.gumeniuk@gmail.com>
2019-08-29 22:46:59 +0300

    Create views to StudyingController actions and appropriate view models

---
+++ Schedule/ViewModels/LeaveTeacherFeedbackViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Schedule.Models;
+using Schedule.Models.Enums;
+using Schedule.Models.JsonHelpers;
+
+namespace Schedule.ViewModels
+{
+    public class LeaveTeacherFeedbackViewModel
+    {        
+        public string TeacherName { get; set; }
+        public Rate Rate { get; set; }
+        public string Feedback { get; set; }
+        public bool IsAnonymus { get; set; }
+    }
+}
---
+++ Schedule/ViewModels/TeacherFeedbackViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Schedule.Models;
+using Schedule.Models.Enums;
+using Schedule.Models.JsonHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Schedule.ViewModels
+{
+    public class TeacherFeedbackViewModel
+    {
+        public User CurrentUser { get; set; }
+        public ResponseTeacherData Teacher { get; set; }
+        public IEnumerable<TeacherFeedback> TeacherFeedbacks { get; set; }
+        public IEnumerable<Rate> Rates { get; } = Enum.GetValues(typeof(Rate)).Cast<Rate>().TakeLast(5);
+    }
+}
