6faf578e8d8eae1c1fda3fa33c052d275460ec42
rusgumeniuk <rus.gumeniuk@gmail.com>
2019-08-29 22:45:44 +0300

    Create TeacherFeedback.cs

---
+++ Schedule/Models/TeacherFeedback.cs
@@ -0,0 +1,13 @@
+﻿using Schedule.Models.Enums;
+using Schedule.Models.JsonHelpers;
+
+namespace Schedule.Models
+{
+    public class TeacherFeedback
+    {
+        public User User { get; set; }
+        public ResponseTeacherData Teacher { get; set; }
+        public string Description { get; set; }
+        public Rate Rate { get; set; } = Rate.Undefined;
+    }
+}
